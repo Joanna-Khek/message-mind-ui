2d51e9b5f29c73a79d9e55608261b7f62540e27a
Joanna <joannakhek@gmail.com>
2025-05-24 14:11:19 +0800

    feat: create completed and incompleted tabs to store respective items

--- app/dashboard/[category]/page.tsx
+++ app/dashboard/[category]/page.tsx
@@ -1,20 +1,37 @@
 import { lowercaseCategory, titleCategory } from "@/utils";
 import { TableReviews } from "@/components/Tables/TableReviews";
 import { fetchData } from "@/utils";
+import { Tabs, TabsList, TabsTab, TabsPanel } from '@mantine/core';
 
 export default async function CategoryPage({ params }: { params: { category: string }}) {
-    const { category } = await params; // Await params to access category
+    const { category } = await params;
     const title = titleCategory(category);
     const formattedTitle = lowercaseCategory(category);
     const items = await fetchData();
 
     const categoryItems = items.filter(item => item.category === formattedTitle);
     
+    const completedItems = categoryItems.filter(item => item.completed);
+    const uncompletedItems = categoryItems.filter(item => !item.completed);
     return (
         <div className="w-full flex justify-center">
             <div className=" w-full p-4 flex flex-col gap-4">
                 <h1 className="text-2xl font-bold">{title}</h1>
-                <TableReviews items={categoryItems} />
+                <Tabs defaultValue="uncompleted">
+                    <TabsList>
+                        <TabsTab value="uncompleted">Uncompleted</TabsTab>
+                        <TabsTab value="completed">Completed</TabsTab>
+                    </TabsList>
+
+                    <TabsPanel value="uncompleted" pt="xs">
+                        <TableReviews items={uncompletedItems}  />
+                    </TabsPanel>
+
+                    <TabsPanel value="completed" pt="xs">
+                        <TableReviews items={completedItems} />
+                    </TabsPanel>
+                </Tabs>
+                
             </div>
         </div>
     )
